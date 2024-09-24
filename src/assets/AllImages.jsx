79120ec279288bc9028633961fe569e5126a86a0
Rabeya akter <rabeyaakterbdcit@gmail.com>
2024-09-24 18:18:20 +0600

    added image

--- src/assets/AllImages.jsx
+++ src/assets/AllImages.jsx
@@ -10,9 +10,35 @@ import product6 from "../assets/images/product6.jpg";
 import user from "../assets/images/user.jpg";
 import arrow from "../assets/images/arrow.png";
 import limitedOffer from "../assets/images/limitedOffer.jpg";
+import headphone1 from "../assets/images/headphone1.jpg";
+import headphone2 from "../assets/images/headphone2.jpg";
+import headphone3 from "../assets/images/headphone3.jpg";
+import headphone4 from "../assets/images/headphone4.jpg";
+import headphone5 from "../assets/images/headphone5.jpg";
+import laptop1 from "../assets/images/laptop1.jpg";
+import laptop2 from "../assets/images/laptop2.jpg";
+import laptop3 from "../assets/images/laptop3.jpg";
+import laptop4 from "../assets/images/laptop4.jpg";
+import camera1 from "../assets/images/camera1.jpg";
+import camera2 from "../assets/images/camera2.jpg";
+import camera3 from "../assets/images/camera3.jpg";
+import camera4 from "../assets/images/camera4.jpg";
+import printer1 from "../assets/images/printer1.jpg";
+import printer2 from "../assets/images/printer2.jpg";
+import printer3 from "../assets/images/printer3.jpg";
+import drone1 from "../assets/images/drone1.jpg";
+import drone2 from "../assets/images/drone2.jpg";
+import drone3 from "../assets/images/drone3.jpg";
+import drone4 from "../assets/images/drone4.jpg";
+import mcp1 from "../assets/images/mcp1.jpg";
+import mcp2 from "../assets/images/mcp2.jpg";
+import mcp3 from "../assets/images/mcp3.jpg";
 
 export const AllImages = {
   logo,
+  mcp1,
+  mcp2,
+  mcp3,
   hero,
   airpods,
   product1,
@@ -24,4 +50,24 @@ export const AllImages = {
   user,
   arrow,
   limitedOffer,
+  headphone1,
+  headphone2,
+  headphone3,
+  headphone4,
+  headphone5,
+  laptop1,
+  laptop2,
+  laptop3,
+  laptop4,
+  camera1,
+  camera2,
+  camera3,
+  camera4,
+  printer1,
+  printer2,
+  printer3,
+  drone1,
+  drone2,
+  drone3,
+  drone4,
 };
